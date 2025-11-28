eeb8ee6a5805c7ec62b883942aaf121c35449c48
nxht <hanguyl91@gmail.com>
2025-11-28 18:40:20 +0900

    chore: update biome.jsonc

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import fs from "node:fs/promises";
-import { generateTheme } from "./theme.ts";
+import fs from 'node:fs/promises';
+import { generateTheme } from './theme.ts';
 
 const theme = generateTheme();
 
-const base_dir = "./themes";
+const base_dir = './themes';
 
 if (!fs.exists(base_dir)) {
-	await fs.mkdir(base_dir);
+  await fs.mkdir(base_dir);
 }
 
 Bun.write(
-	`${base_dir}/one-monokai-python.json`,
-	JSON.stringify(theme, null, 2),
+  `${base_dir}/one-monokai-python.json`,
+  JSON.stringify(theme, null, 2),
 );
